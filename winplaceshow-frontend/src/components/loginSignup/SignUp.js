cdec9c53b51695323aeaca42870d5ebf09622756
Anubhav311 <anubhavbhambri1@gmail.com>
2019-04-19 03:52:10 +0530

    Completed styling of update-user-details page.

--- winplaceshow-frontend/src/components/loginSignup/SignUp.js
+++ winplaceshow-frontend/src/components/loginSignup/SignUp.js
@@ -51,6 +51,7 @@ const SignupButton = styled.button`
     font-weight: bold;
     height: 40px;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    cursor: pointer;
 
 `
 
--- winplaceshow-frontend/src/components/loginSignup/login.js
+++ winplaceshow-frontend/src/components/loginSignup/login.js
@@ -52,6 +52,7 @@ const LoginButton = styled.button`
     font-weight: bold;
     height: 40px;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.2);
+    cursor: pointer;
 `
 
 const LoginDiv = styled.div`
